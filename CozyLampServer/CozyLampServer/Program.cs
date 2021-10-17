404eb8880eaa0469ebef302b67d9e0ef5db7fe41
MaxThom <maxthomassin@hotmail.com>
2021-10-17 20:31:34 +1300

    Init webapi

---
+++ CozyLampServer/CozyLampServer/Program.cs
@@ -0,0 +1,54 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+
+/*
+ * 
+ * Each device has a connection key (that can be modified on the esp32 web ui).
+ * On ON, the lamp send a request saying its on, the api returns if another device in the group is ON.
+ * If so, the device change to the ThinkOfYou light.
+ * On OFF, the lamp send a request saying its off.
+ * Every 5 seconds, a lamp in ON state send the ON request. The API must detect the timeout if one lamp doesnt send its refresh and set it to OFF.
+ * The dictonary holding the connection key must be deleted if all devices are off and will be recreated on ON request.
+ */
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+       new WeatherForecast
+       (
+           DateTime.Now.AddDays(index),
+           Random.Shared.Next(-20, 55),
+           summaries[Random.Shared.Next(summaries.Length)]
+       ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+app.Run();
+
+internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
